caef970e170ccd3114b0a317bf470ea2c50a715d
Jason Green <nogeek@Unknown-00-23-6c-88-75-08.lan>
2009-06-04 16:19:54 +0100

    adding production deprec stuff:

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,6 @@
-set :stages, %w(staging production)
-
-set :default_stage, "staging"
+require 'deprec'
 require 'capistrano/ext/multistage'
+set :stages, %w(staging production)
+set :default_stage, "staging"
 
-
-
-
-set :repository,  "git@github.com:hookercookerman/cnn-airport-cal.git"
-set :scm, "git"
-set :git_enable_submodules,1
-set :user, "deploy"
-set :password, "80shour2"
-set :git_shallow_clone, 1 # We don't need the full history
-set :deploy_via, :remote_cache
-ssh_options[:forward_agent] = true
-
-
-
-# Some weird glitch with -u app
-set :runner, nil
-
-namespace :deploy do
-  task :tail_log do
-    run "tail -f #{shared_path}/log/production.log"
-  end
-end
+  
