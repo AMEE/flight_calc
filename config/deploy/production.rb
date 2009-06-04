d6cda9885320bd1af1576a627b1e2a4cc7af4666
Jason Green <nogeek@Unknown-00-23-6c-88-75-08.lan>
2009-06-04 17:50:28 +0100

    adding sphinc stuff

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,29 +1,14 @@
+# Domain and Application settings
 set :application, "cnn-carbon-calculator"
 set :domain, "cnn.dynamic50.com"
-
-namespace :deprec do
-  namespace :memcache do
-    set :memcache_ip, '127.0.0.1'
-    set :memcache_port, 11211
-    set :memcache_memory, 64
-  end
-end
-
-   
-set :ruby_vm_type,      :ree        # :ree, :mri
-set :web_server_type,   :apache     # :apache, :nginx
-set :app_server_type,   :passenger  # :passenger, :mongrel
-set :db_server_type,    :mysql      # :mysql, :postgresql, :sqlite
-
-# set :packages_for_project, %w(libmagick9-dev imagemagick libfreeimage3) # list of packages to be installed
-
-set :gems_for_project, %w(treetop json_pure nokogiri faker rack_hoptoad haml mislav-will_paginate rack hookercookerman-amee httparty wycats-moneta) # list of gems to be installed
 
 role :app, "cnn.dynamic50.com"
 role :web, "cnn.dynamic50.com"
 role :db,  "cnn.dynamic50.com", :primary => true
 role :mail, "cnn.dynamic50.com"
 role :sphinx, "cnn.dynamic50.com"
+
+
 
 
 namespace :deploy do
