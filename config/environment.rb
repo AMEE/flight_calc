348c12ad97f5b07e8ceddf6bc7738b09bd10bb50
HOOKERCOOKERMAN <richard.hooker@dynamic50.com>
2009-06-05 15:21:14 +0100

    FUCK RACK

--- config/environment.rb
+++ config/environment.rb
@@ -12,7 +12,6 @@ Rails::Initializer.run do |config|
   config.gem 'faker', :lib => 'faker', :version => '~> 0.3.1'
   config.gem 'haml', :lib => 'haml'
   config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com', :version => '~> 2.2.3'
-  config.gem 'rack', :version => '~> 1.0'
   config.gem 'hookercookerman-amee', :lib => 'amee'
   config.gem "httparty", :version => '~> 0.4.3'
   config.gem "wycats-moneta", :lib => "moneta", :source => 'http://gems.github.com'
