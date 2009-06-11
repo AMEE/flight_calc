909f350966504b3ee963d8308ca308c1c06b1897
Jason Green <nogeek@Unknown-00-23-6c-88-75-08.lan>
2009-06-11 13:18:30 +0100

    moving to staging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,13 @@ config.action_view.cache_template_loading            = true
 ::Amee::Config.set do |config|
   config[:username] = "cnn"
   config[:password] = "aa221361"
-  config[:server] = "live.amee.com"
+  config[:server] = "stage.amee.com"
 end
+
+# ::Amee::Config.set do |config|
+#   config[:username] = "cnn"
+#   config[:password] = "aa221361"
+#   config[:server] = "live.amee.com"
+# end
 
 CNN_AMEE_PROFILE = "9D7C819AB003"
