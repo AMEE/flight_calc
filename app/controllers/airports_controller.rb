64657dfea5c0549385ff6a7900fb0967905373fc
James Smith <james@floppy.org.uk>
2009-10-08 16:00:59 +0100

    Fix airport search - committed too early last time, forgot what I was doing

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -3,8 +3,8 @@ class AirportsController < ApplicationController
   def search
     if !params[:airport].blank?
       @airports = Rails.cache.fetch("airport-search-#{params[:airport].gsub(/\W/,"-")}") do 
-        query = "'#{params[:airport]}%'"
-        airports = Airport.find(:all, :conditions => "name LIKE :query OR country LIKE :query OR country LIKE :query OR iata_code LIKE :query")
+        query = "#{params[:airport]}%"
+        airports = Airport.find(:all, :conditions => ["name LIKE :query OR country LIKE :query OR country LIKE :query OR iata_code LIKE :query", {:query => query}])
         airports.map{|airport| [:id => airport.id, :address => airport.display]}.flatten.to_json
       end
       render :json => @airports
