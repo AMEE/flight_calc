aa29f50c19b2902e134ab1b59f03f6bbd00760d4
James Smith <james@Marvin.(none)>
2010-02-10 16:16:27 +0000

    clean up dead code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,8 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  # before_filter :authenticate_global
   self.allow_forgery_protection = false
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
-  
-  
-  protected
-  def authenticate_global
-    if ['production'].include?(Rails.env)
-      authenticate_or_request_with_http_basic do |user_name, password|
-        user_name == "admin" && password == "cnn"
-      end
-    end
-  end
+ filter_parameter_logging :password
+    
 end
--- app/models/cnn_user.rb
+++
@@ -1,3 +0,0 @@
-class CnnUser
-
-end
